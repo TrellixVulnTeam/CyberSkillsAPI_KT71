2e885072abf773e33af7c689ad0efacbfc73dfbf
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-23 21:25:16 +0100

    Add functions for downloading the geckodriver

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ import time
 # For handling the folders
 import os
 
+# For downloading the geckodriver
+import requests
+
+# For extracting the geckodriver
+import tarfile
+
 import re
 
 filePathPattern = re.compile('[^a-zA-Z0-9]')
@@ -42,6 +48,21 @@ def createFolder(folderName):
             os.chmod(Path("./" + folderName), 0o755)
         except:
             raise Exception("Failed to set the 755 permissions on {}, either remove the folder or set the right perms yourself and try again.".format(folderName))
+
+# Mozilla will have an api endpoint giving a lot of information about the latest releases for the geckodriver, from which the url for the linux 64 bit has to be extracted
+def extractDriverURL():
+    driverDetails = json.loads(requests.get("https://api.github.com/repos/mozilla/geckodriver/releases/latest").text)
+
+    for platformRelease in driverDetails['assets']:
+        if platformRelease['name'].endswith("linux64.tar.gz"):
+            return platformRelease['browser_download_url']
+
+# Downloading and extracting the .tar.gz file the geckodriver is stored in into the tools directory
+def downloadDriver(driverURL):
+    driverContents = requests.get(driverURL, stream=True)
+    with tarfile.open(fileobj=driverContents.raw, mode='r|gz') as driverFile:
+        driverFile.extractall(path=Path("./"))
+
 
 def writeEventsToFile(year, month, contents):
     year = filePathPattern.sub("", year)
