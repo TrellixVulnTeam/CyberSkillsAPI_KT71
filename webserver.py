37c38ff0b34e21b6804c55daaa08318e29e4f48a
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-23 21:44:48 +0100

    Don't host on all interfaces in debug mode

--- webserver.py
+++ webserver.py
@@ -27,4 +27,4 @@ def sendEventsJSON(year, month):
         return "Didn't recognize either year or month", 422
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
